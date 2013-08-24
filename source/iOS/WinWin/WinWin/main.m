84e5924cea4d21ab5f3f87ae29bf028b08d0ad6c
Serdar Karatekin <karatekinserdar@gmail.com>
2013-08-24 01:01:30 -0400

    Adding initial iOS project

---
+++ source/iOS/WinWin/WinWin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WinWin
+//
+//  Created by Serdar Karatekin on 8/24/13.
+//  Copyright (c) 2013 Serdar Karatekin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
